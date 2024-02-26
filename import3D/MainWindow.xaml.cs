70a33896f6bfd0c9d4564032d2e35bad5afa4d4d
JackCao <0512mv@163.com>
2024-02-26 09:37:55 +0800

    优化

--- import3D/MainWindow.xaml.cs
+++ import3D/MainWindow.xaml.cs
@@ -10,16 +10,16 @@ namespace import3D
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ModelImporter import = new ModelImporter();//导入模型的类对象
+        private readonly ModelImporter import = new ModelImporter();//导入模型的类对象
 
-        public readonly string myModelPath = "3D_Models";//模型文件夹路径
+        public readonly string ModelForder = "3D_Models";//模型文件夹
 
         public MainWindow()
         {
             InitializeComponent();
             ObjReader myHelixObjReader = new ObjReader();
             //读入模型文件
-            Model3DGroup MyModel = import.Load($@".\{myModelPath}\magnolia.stl");
+            Model3DGroup MyModel = import.Load($@".\{ModelForder}\magnolia.stl");
             // Display the model
             model.Content = MyModel;
             helixControl.ZoomExtents();
@@ -28,10 +28,9 @@ namespace import3D
         private void F_16_btn_Click(object sender, RoutedEventArgs e)
         {
             string myModelPath = chooseModel();
-            Model3DGroup MyModel;
             if (string.Empty != myModelPath)
             {
-                MyModel = import.Load(myModelPath);
+                var MyModel = import.Load(myModelPath);
                 model.Content = MyModel;
                 helixControl.ZoomExtents();
             }
@@ -41,9 +40,9 @@ namespace import3D
         {
             string path = string.Empty;
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            dlg.InitialDirectory = Path.Combine(System.IO.Directory.GetCurrentDirectory(), myModelPath);
+            dlg.InitialDirectory = Path.Combine(Directory.GetCurrentDirectory(), ModelForder);
             // Show open file dialog box
-            System.Nullable<bool> result = dlg.ShowDialog();
+            bool? result = dlg.ShowDialog();
 
             // Process open file dialog box results
             if (result == true)
