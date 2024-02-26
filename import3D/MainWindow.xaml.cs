b78df8992a6c402a1289065258fca44c103b79fb
JackCao <0512mv@163.com>
2024-02-26 11:44:45 +0800

    支持加载多个

--- import3D/MainWindow.xaml.cs
+++ import3D/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using HelixToolkit.Wpf;
+﻿using System.Collections.Generic;
+using HelixToolkit.Wpf;
 using System.IO;
 using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
 
 namespace import3D
 {
@@ -27,13 +30,12 @@ namespace import3D
 
         private void F_16_btn_Click(object sender, RoutedEventArgs e)
         {
-            string myModelPath = chooseModel();
-            if (string.Empty != myModelPath)
+            var myModelPath = chooseModel();
+            if (myModelPath.Count > 0)
             {
-                var MyModel = import.Load(myModelPath);
-
-                if (chkMult.IsChecked == true)
+                foreach (var path in myModelPath)
                 {
+                    var MyModel = import.Load(path);
                     // 创建一个新的ModelVisual3D对象，并将其Content属性设置为新加载的模型
                     ModelVisual3D modelVisual3D = new ModelVisual3D();
                     modelVisual3D.Content = MyModel;
@@ -41,30 +43,49 @@ namespace import3D
                     // 将新的ModelVisual3D对象添加到ModelVisual3D容器中
                     model.Children.Add(modelVisual3D);
                 }
-                else
-                {
-                    model.Content = MyModel;
-                }
-
                 helixControl.ZoomExtents();
             }
+
+            //只能选择一个模型
+            //if (string.Empty != myModelPath)
+            //{
+            //    var MyModel = import.Load(myModelPath);
+
+            //    if (chkMult.IsChecked == true)
+            //    {
+            //        // 创建一个新的ModelVisual3D对象，并将其Content属性设置为新加载的模型
+            //        ModelVisual3D modelVisual3D = new ModelVisual3D();
+            //        modelVisual3D.Content = MyModel;
+
+            //        // 将新的ModelVisual3D对象添加到ModelVisual3D容器中
+            //        model.Children.Add(modelVisual3D);
+            //    }
+            //    else
+            //    {
+            //        model.Content = MyModel;
+            //    }
+
+            //    helixControl.ZoomExtents();
+            //}
         }
 
-        private string chooseModel()
+        private List<string> chooseModel()
         {
-            string path = string.Empty;
+            List<string> filesPath = new List<string>();
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Multiselect = true;
+            dlg.FileName = "DinoRider.3ds"; // Default file name
             dlg.InitialDirectory = Path.Combine(Directory.GetCurrentDirectory(), ModelForder);
             // Show open file dialog box
             bool? result = dlg.ShowDialog();
 
             // Process open file dialog box results
-            if (result == true)
+            if (result == true && dlg.FileNames.Length > 0)
             {
                 // Open document
-                path = dlg.FileName;
+                filesPath.AddRange(dlg.FileNames);
             }
-            return path;
+            return filesPath;
         }
     }
 }
